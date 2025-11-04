eb2fa9c850eb01acade3ebe324b2f0b60feb7006
Lisa12344321 <lisa.alfven@elev.ga.ntig.se>
2025-11-04 13:39:23 +0100

    påse

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 run = True
 
+antal = 0
+
 bag = []
 
 while run:
@@ -7,16 +9,34 @@ while run:
     print("Visa innehållet i påsen [V]")
     print("Spara i påsen [S]")
     print("Avsluta programmet [Q]")
+    print("Ta bort något från påsen [R]")
+    print()
     choice = input("Välj: ")
 
     if choice.lower() == "v":
         print("*Öppnar påsen*")
         print("[", end='')
+
         for thing in bag:
-            print(f"{thing}, ", end='')
+            print(f" {thing} ", end='')
         print("]")
+
     elif choice.lower() == "s":
-        bag.append(input("Skriv vad du vill spara: "))
+        if antal <= 10:
+            bag.append(input("Skriv vad du vill spara: "))
+            antal += 1
+        else:
+            print("För många saker :(")
+
+    elif choice.lower() == "r":
+        print("Påsens innehåll: ")
+        print("[", end='')
+        for thing in bag:
+            print(f" {thing} ", end='')
+        print("]")
+ 
+        bag.pop(int(input("Ta bort saken på plats nr (första är 0): ")))
+
     elif choice.lower() == "q":
         run = False
     else:
